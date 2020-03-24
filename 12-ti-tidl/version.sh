688d1ada23935e856b088d5c8a2cd46733594de9
Robert Nelson <robertcnelson@gmail.com>
2020-03-24 16:08:58 -0500

    add: ti-tidl
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ 12-ti-tidl/version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+#https://github.com/rcn-ee/tidl-api/tree/v01.04.00-bb.org
+
+package_name="ti-tidl"
+debian_pkg_name="${package_name}"
+package_version="01.04.00-bb.org-0.0"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}_${package_version}"
+
+git_repo="https://github.com/rcn-ee/tidl-api.git"
+git_sha="6461a1bdb7a270c89b92ddbcd3329b2ec482b3ca"
+reprepro_dir="o/${package_name}"
+dl_path=""
+
+debian_version="${package_version}-0rcnee1"
+debian_patch=""
+debian_diff=""
+
+buster_version="~buster+20200324"
